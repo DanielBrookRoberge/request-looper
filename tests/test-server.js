7b03f113ae33eb4c3ada777ff0bc4d0287dc0cfc
Daniel Brook-Roberge <d.brook.roberge@gmail.com>
2015-04-20 21:59:44 -0700

    Add route for basic tests.

--- tests/test-server.js
+++ tests/test-server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
-
+app.get('/api1/:n', function(req, res) {
+  res.send([{test: 'test'}, {test: 'test1'}, {test: 'test2'}]);
+});
 
 app.listen(2222);
